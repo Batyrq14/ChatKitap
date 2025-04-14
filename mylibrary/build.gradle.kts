ecff55ab56e3e3ae56715a6bff7ffda3cf4c7181
BatyrCode <utegenov1403@gmail.com>
2025-04-14 06:12:46 +0500

    Made ChatLibrary public and updated to 1.0.2

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -56,7 +56,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.Batyrq14"
             artifactId = "mylibrary"
-            version = "1.0.1"
+            version = "1.0.2"
             afterEvaluate {
                 from(components["release"])
             }
--- mylibrary/src/main/java/com/example/mylibrary/ChatLibrary.kt
+++ mylibrary/src/main/java/com/example/mylibrary/ChatLibrary.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import com.example.mylibrary.ChatActivity
 
-class ChatLibrary private constructor() {
+public class ChatLibrary private constructor() {
 
     companion object {
         /**
